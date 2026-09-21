fcbb0e7e70e9c282d252902a56ccd638a2cc6311
Kawaii_ttt <3313251488@qq.com>
2026-09-21 11:15:56 +0800

    基本参数的赋值与打印

---
+++ C - C++/dengji.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int Age = 20, Num = 1101055;
+    char Sex = 'M';
+    char name[20] = "张三";
+    float Score = 209.5;
+    printf("姓名=%s 年龄=%d 学号=%d 性别=%c 成绩=%0.1f\n", name, Age, Num, Sex, Score);
+}
